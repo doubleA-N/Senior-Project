9e240a833a7f3f4a9c2ed72e27f49bca402523f4
doubleA-N <Aaeyysm@gmail.com>
2020-10-11 17:42:24 +0700

    commit all file

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './CSS/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Sidebar from './components/Sidebar';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import * as Testdata from "./data/Testdata.json";
+
+function App() {
+  const [activepoint, setActivepoint] = React.useState(null);
+ 
+  return (
+    <div className='App container-fluid'>
+      <h3 className='App-link'>หน้าแรก</h3>
+      <hr className='App-line'></hr>
+    <Sidebar />
+    <Map center={[13.76, 100.51]} zoom={6}>
+    {Testdata.features.map(point => (
+        <Marker
+          key={point.properties.USER_ID}
+          position={[
+            point.geometry.coordinates[1],
+            point.geometry.coordinates[0]
+          ]}
+          onClick={() => {
+            setActivepoint(point);
+          }}
+        />
+      ))}
+
+{activepoint && (
+    <Popup
+      position={[
+        activepoint.geometry.coordinates[1],
+        activepoint.geometry.coordinates[0]
+      ]}
+      onClose={() => {
+        setActivepoint(null);
+      }}
+    >
+      <div>
+        <h2>{activepoint.properties.NAME}</h2>
+        <p>{activepoint.properties.DESCRIPTION}</p>
+      </div>
+    </Popup>
+  )}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../CSS/App.css';
+import '../CSS/Sidebar.css';
+import img from '../img/hipster.png';
+import folder from '../img/folder.png';
+import report from '../img/write-letter.png';
+import list from '../img/listing-option.png';
+import analyze from '../img/analytics.png';
+import setting from '../img/settings.png';
+
+class sidebar extends React.Component {
+    render() {
+        return (
+            <div>
+                <div class="sidenav">
+                <img class ='img-sidebar' src={img}/>
+                <p class ='profile-name'>UserName</p>
+                <a class ='profile-sidebar'href="#">โปรไฟล์ของคุณ</a>
+                <a class ='sidebar-link' href="/"><img class ='logo' src={folder}/>หน้าแรก</a>
+                <a class ='sidebar-link' href="#"><img class ='logo' src={report}/>แจ้งปัญหา</a>
+                <a class ='sidebar-link' href="#"><img class ='logo' src={list}/>ดูข้อมูลของปัญหา</a>
+                <a class ='sidebar-link' href="#"><img class ='logo' src={analyze}/>วิเคราะห์ปัญหา</a>
+                <a class ='sidebar-link' href="#"> <img class ='logo' src={setting}/>การตั้งค่า</a> 
+                <a href="something" class="button3">ออกจากระบบ</a>
+
+                </div>  
+            </div>
+            
+        );
+    }
+}
+
+export default sidebar;
