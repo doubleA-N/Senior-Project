0ffd4c0e69162e40a1f094c285b5d74d0cf819b4
doubleA-N <Aaeyysm@gmail.com>
2020-10-21 23:40:09 +0700

    set firebase and adapt/connect map with firebase

--- src/App.js
+++ src/App.js
@@ -1,64 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CSS/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Sidebar from './components/Sidebar';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
-import * as Testdata from "./data/Testdata.json";
+import MarkerClusterGroup from "react-leaflet-markercluster";
+// import * as Testdata from "./data/Testdata.json";
+
+import { db, auth } from './data/firebase'
 
 function App() {
-  const [activepoint, setActivepoint] = React.useState(null);
+
+  const [problems, setProblems] = useState(null)
+  const [popup, setPopup] = useState(null)
   const map = new Icon({
     iconUrl: "/marker.svg",
-    iconSize: [30 , 30]
+    iconSize: [30, 30]
   });
- 
+
+  // useEffect(() => {
+  //   console.log('problem', problems)
+  // }, [problems])
+
+  function setActiveProblem(problem_id) {
+    console.log(problem_id, 'set active problem')
+    setProblems(prev => prev.map(el => {
+      if (el.problem_id === problem_id) {
+        return { ...el, popup: true }
+      }
+      return { ...el, popup: false }
+    }))
+  }
+
+  useEffect(() => {
+    db.collection('problems')
+      .get()
+      .then(snapshot => {
+        const problems_data = []
+        snapshot.forEach(doc => {
+          const data = doc.data()
+          problems_data.push(data)
+        })
+        setProblems(problems_data.map(el => ({ ...el, popup: false })))
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className='App'>
       <h3 className='App-link'>หน้าแรก</h3>
+      <form>
+        <input class='input' type="text" name="search" placeholder="Search.." />
+      </form>
       <hr className='App-line'></hr>
-    <div>
-    <form>
-        <input class='input' type="text" name="search" placeholder="Search.."/>
-        </form>
-    </div>
-    <Sidebar />
-    <Map center={[13.76, 100.51]} zoom={6}>
-    {Testdata.features.map(point => (
-        <Marker
-          key={point.properties.USER_ID}
-          position={[
-            point.geometry.coordinates[1],
-            point.geometry.coordinates[0]
-          ]}
-          onClick={() => {
-            setActivepoint(point);
-          }}
-          icon={map}
-        />
-      ))}
+      <Sidebar />
 
-{activepoint && (
-    <Popup
-      position={[
-        activepoint.geometry.coordinates[1],
-        activepoint.geometry.coordinates[0]
-      ]}
-      onClose={() => {
-        setActivepoint(null);
-      }}
-    >
-      <div>
-        <h2>{activepoint.properties.NAME}</h2>
-        <p>{activepoint.properties.DESCRIPTION}</p>
-      </div>
-    </Popup>
-  )}
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
+      <Map center={[13.76, 100.51]} zoom={5}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <MarkerClusterGroup>
+          {problems && problems.length != 0 && problems.map(problem => {
+            // console.log(problem.problem_id + String(problem.popup))
+            return (
+              <Marker
+                key={problem.problem_id}
+                position={[
+                  problem.location.latitude,
+                  problem.location.longitude
+                ]}
+                onClick={() => {
+                  setPopup(problem)
+                }}
+                icon={map}
+              >
+              </Marker>
+            );
+          })}
+        </MarkerClusterGroup>
+        {
+          popup && (
+            <Popup
+              position={[
+                popup.location.latitude,
+                popup.location.longitude,
+              ]
+              }
+              onClose={() => {
+                setPopup(null);
+              }}
+            >
+              <div>
+                <h2>{popup.name}</h2>
+                <p>{popup.description}</p>
+              </div>
+            </Popup>
+          )
+        }
+      </Map>
 
     </div>
   );
---
+++ src/data/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRljzIEaVg28y4Qn1-HDrRJoH-53O1cpA",
+    authDomain: "senior-project-3f176.firebaseapp.com",
+    databaseURL: "https://senior-project-3f176.firebaseio.com",
+    projectId: "senior-project-3f176",
+    storageBucket: "senior-project-3f176.appspot.com",
+    messagingSenderId: "1052317350942",
+    appId: "1:1052317350942:web:b65f31b59477b1c470020f",
+    measurementId: "G-H0F8TQCRPZ"
+  }
+
+  firebase.initializeApp(firebaseConfig)
+
+  export const auth = firebase.auth()
+  export const db = firebase.firestore()
+
+  export default firebase
